a9b716a5d06e97414999ab6a90d4384361f15b95
ainyan4645 <merytoku4869ws@gmail.com>
2025-08-07 06:59:48 +0900

    0807Mac

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,15 +5,76 @@
 @endsection
 
 @section('content')
-<div class="content">
-    <div class="content__form">
-        <form class="form" action="/todos" method="post">
-            <input class="form__text" type="text" name="content" />
-            <button class="form__button" type="submit">作成</button>
-        </form>
-    </div>
 
-    <div class="content__todo-group">
+<div class="todo__alert">
+    <div class="todo__alert--success">
+        Todoを作成しました
+    </div>
+</div>
+
+<div class="todo__content">
+    <form class="create-form" action="/todos" method="post">   <!-- action,methodこれから -->
+        <div class="create-form__item">
+            <input class="create-form__item-input" type="text" name="content" />
+        </div>
+        <div class="create-form__button">
+            <button class="form__button-submit" type="submit">作成</button>
+        </div>
+    </form>
+
+    <div class="todo-table">
+        <table class="todo-table__inner">
+            <tr class="todo-table__row">
+                <th class="todo-table__header">Todo</th>
+            </tr>
+            <tr class="todo-table__row">
+                <td class="todo-table__item">
+                    <form class="update-form">
+                        <div class="update-form__item">
+                            <input class="update-form__item-input" type="text" name="content" value="test">
+                        </div>
+                        <div class="update-form__button">
+                            <button class="update-form__button-submit" type="submit">更新</button>
+                        </div>
+                    </form>
+                </td>
+                <td class="todo-table__item">
+                    <form class="delete-form">
+                        <div class="delete-form__button">
+                            <button class="delete-form__button-submit" type="submit">削除</button>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+            <tr class="todo-table__row">
+                <td class="todo-table__item">
+                    <form class="update-form">
+                        <div class="update-form__item">
+                            <input class="update-form__item-input" type="text" name="content" value="test2">
+                        </div>
+                        <div class="update-form__button">
+                            <button class="update-form__button-submit" type="submit">更新</button>
+                        </div>
+                    </form>
+                </td>
+                <td class="todo-table__item">
+                    <form class="delete-form">
+                        <div class="delete-form__button">
+                            <button class="delete-form__button-submit" type="submit">削除</button>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+@endsection
+
+
+<!-- 自分で作成 -->
+<!--
+    <div class="todo-group">
+        <table>
         <div class="todo-group__heading">
             <h2>Todo</h2>
         </div>
@@ -21,5 +82,4 @@
             //追加されるtodoについて記述
         </div>
     </div>
-</div>
-@endsection
+-->
